3a1c7ea51d30d26e87c1bf8b08ca0061b09c36ca
Rhara Ianna <rharaianna@gmail.com>
2023-11-30 17:31:53 -0300

    atualiza mainjs com destino

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 document.entrada.addEventListener('submit', leFormulario);
 
+const olJoao = document.querySelector("ol#joao");
+const olMaria = document.querySelector("ol#maria");
+const olPomar = document.querySelector("ol#pomar");
+
 function leFormulario(event) {
 
     event.preventDefault()
    
-    const quantidade = document.entrada.quantidade.value;
+    const quantidade = document.entrada.quantidade.valueAsNumber;
     const fruta = document.entrada.fruta.value;
     const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
 
-console.log(`${origem} doa ${quantidade} ${fruta}`);
+console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 }
